158cd8f29d1d7ce98349f52a67d5d564d1f87d13
HelloBlue-stuff <bash-send-setup@duck.com>
2024-11-10 11:15:18 +1100

    Update knownHosts.ts

--- knownHosts.ts
+++ knownHosts.ts
@@ -5,6 +5,7 @@
  */
 
 export const knownHosts = [
+    
     "rickroll.link",
     "rickrolled.fr",
     "my-cool-app.com",
@@ -72,6 +73,7 @@ export const knownHosts = [
     "technocolbert.co.nr",
     "techsmartly.net",
     "thekickback.com/rickroll/rickroll.php",
+    "tinyurl.com/interfont,
     "tinyurl.com/244u3n",
     "tinyurl.com/2nmscj",
     "tinyurl.com/2tcnbl",
